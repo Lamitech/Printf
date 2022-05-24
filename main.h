be47d102b29f7f5bf6140713e208a0d2b1ccd3e4
thebillionairepastor <101338709+thebillionairepastor@users.noreply.github.com>
2022-05-24 18:32:29 +0100

    Update main.h

--- main.h
+++ main.h
@@ -32,7 +32,6 @@ int int_length(unsigned int n);
 int get_exponent(int len);
 int _print_binary(va_list);
 Int print_R(va_list arg)
-
 int binary_len(unsigned int n);
 char *rev_string(char *);
 void _putbase(char *str);
